bfbf0f9099b520d681293a84992e238fdb75a2fd
moisesvi <moisesvargasms@gmail.com>
2026-03-13 18:04:45 -0500

    alerta y modal

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import { Bell, Search, User, ChevronDown } from 'lucide-react';
-import { usePathname } from 'next/navigation';
+import { redirect, usePathname } from 'next/navigation';
 
 export function Topbar() {
   const pathname = usePathname();
@@ -21,6 +21,10 @@ export function Topbar() {
     }
   };
 
+  const handleRedirectalerts = () => {
+    redirect('/alerts');
+  }
+
   return (
     <header className="h-20 bg-white border-b border-slate-200 flex items-center justify-between px-8 sticky top-0 z-40 ml-64">
       <div className="flex flex-col">
@@ -38,7 +42,7 @@ export function Topbar() {
           />
         </div>
 
-        <button className="relative p-2 text-slate-500 hover:bg-slate-100 rounded-full transition-colors">
+        <button onClick={handleRedirectalerts} className="relative p-2 text-slate-500 hover:bg-slate-100 rounded-full transition-colors">
           <Bell className="w-5 h-5" />
           <span className="absolute top-2 right-2 w-2 h-2 bg-brand-danger rounded-full border-2 border-white"></span>
         </button>
